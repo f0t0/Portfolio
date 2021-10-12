0935fcc032fb2aedb9cf1d88ee26b070cbf2fffb
f0t0 <44043154+f0t0@users.noreply.github.com>
2021-10-12 00:09:02 -0700

    Create Tableau Covid SQL Queries.sql

---
+++ Tableau Covid SQL Queries.sql
@@ -0,0 +1,6 @@
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
